d3131d8f2577161d249d64671281565d6d4d669e
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-05-26 14:41:30 -0600

    Added code that reads and writes info into the json file

--- Develop/server.js
+++ Develop/server.js
@@ -1,29 +1,68 @@
 const express = require('express');
-const path = require('path');
-const api = require('./routes/index');
-
+const fs = require('fs');
 const PORT = 3001;
+const path = require('path');
+const dbJson = require('./db/db.json');
+const uuid = require('./helpers/id_gen');
+
 
 const app = express();
-
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
-//What does this do? like what is the difference with the code above.
-app.use(express.static('public'));
-
-//Why does this does not have a callback function like the other one in notes?
 app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/index.html'))  
+    res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+    res.json(dbJson);
+});
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title, 
+            text,
+            review_id: uuid(),
+        }
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if(err) {
+                console.log(err);
+            } else {
+                
+                const parsedNote = JSON.parse(data);
+                parsedNote.push(newNote);
+
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNote, null, 4), (writeErr) =>
+                    writeErr ? console.error(writeErr) : console.info('Succesfully added note!')
+                );
+            };
+        })
+
+        const response = {
+            status: 'succes',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error posting new note');
+    }
+
+    
+});
 
 app.listen(PORT, () => 
-    console.log(`App listening at http://localhost:${PORT} 🚀 `)
+    console.info(`Server listening at http://localhost:${PORT}`)
 );
-
-
